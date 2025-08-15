901cbd91a5bcf85564b8b38c9e0b572dd928304f
sudhanshu khosla <sudhanshukhosla123@gmail.com>
2025-08-15 10:54:51 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen w-screen  ">
-      <h1 className="">Welcome to HireLog</h1>
+    <div className="bg-black flex items-center justify-center h-screen w-screen  ">
+      <h1 className="text-white text-7xl">Welcome to HireLog</h1>
     </div>
   );
 }
